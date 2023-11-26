fa51ff46f7374c5921f952376fc9173a6e2cfdf3
KwameNtaadu007 <kwamealabama@gmail.com>
2023-11-26 00:21:43 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
+exec { 'install_flask_2.1.0':
   command     => '/usr/bin/pip3 install Flask==2.1.0',
   path        => ['/usr/bin'],
   environment => ['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8'],
   unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require     => Package['python3-pip'],
 }
 
