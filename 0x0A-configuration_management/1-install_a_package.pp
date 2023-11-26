9db51eacd77d8d129238ba9901122f73663c6197
KwameNtaadu007 <kwamealabama@gmail.com>
2023-11-26 00:18:17 -0800

    updated install flask code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Configuration to install python flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => ['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  require     => Package['python3-pip'],
+}
+
