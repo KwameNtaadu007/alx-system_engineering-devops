6f10477a55bb0b9e0ffa69cc7c6e069fcc0155e4
KwameNtaadu007 <kwamealabama@gmail.com>
2023-11-26 00:30:34 -0800

    refactor

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-exec { 'install_flask_2.1.0':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin'],
-  environment => ['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8'],
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# Installing flask from pip3 using puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
